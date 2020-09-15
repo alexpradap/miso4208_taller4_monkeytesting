692a258440370d09ccd20881716ddff185f2202b
Alexander Prada Perez <alexpradap@alexpradap-MacBook.local>
2020-09-15 10:40:10 -0500

    Ejercicio monkey testing con cypress taller 4

---
+++ cypress/integration/monkey_testing_ripper.spec.js
@@ -0,0 +1,84 @@
+describe('Wikipedia under monkeys', function() {
+    it('visits wikipedia and survives monkeys', function() {
+        cy.visit('https://www.wikipedia.org/');
+        cy.wait(1000);
+        randomAction(10);
+    })
+})
+function randomAction(monkeys) {
+
+    function randomEvents (selectors, monkeys) {
+        let randomEvents = [];
+        for (let i = 0; i < monkeys; i++) {
+          randomEvents.push(selectors[getRandomInt(0, selectors.length)]);
+        }
+        console.log(randomEvents);
+        return randomEvents;
+    }
+
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;
+    };
+
+    let selectors = ['a', 'button', 'select', 'input[type=text], input[type=search]'];
+    randomEvents(selectors, monkeys).forEach(selector => {
+        if (selector == 'a') {
+            cy.get('body').then($body => {
+                if ($body.find(selector).length > 0) {
+                    cy.get(selector).then($events => {
+                        var randomEvent = $events.get(getRandomInt(0, $events.length));
+                        if(!Cypress.dom.isHidden(randomEvent)) {
+                            cy.wrap(randomEvent).click({force: true});
+                        }
+                        cy.wait(500);
+                    });
+                }
+            });
+        }
+        else if (selector == 'button') {
+            cy.get('body').then($body => {
+                if ($body.find(selector).length > 0) {
+                    cy.get(selector).then($events => {
+                        var randomEvent = $events.get(getRandomInt(0, $events.length));
+                        if(!Cypress.dom.isHidden(randomEvent)) {
+                            cy.wrap(randomEvent).click({force: true});
+                        }
+                        cy.wait(500);
+                    });
+                }
+            });
+        }
+        else if (selector == 'select') {
+            cy.get('body').then($body => {
+                if ($body.find(selector).length > 0) {
+                    cy.get(selector).then($events => {
+                        var randomEvent = $events.get(getRandomInt(0, $events.length));
+                        cy.wrap(randomEvent).get('option').then($options => {
+                            var randomOption = $options.get(getRandomInt(0, $options.length));
+                            cy.wrap(randomOption)
+                            .invoke('attr', 'value')
+                            .then($value => {
+                                cy.wrap(randomEvent).select($value);
+                            });
+                        });
+                        cy.wait(500);
+                    });
+                }
+            });
+        }
+        else if (selector == 'input[type=text], input[type=search]') {
+            cy.get('body').then($body => {
+                if ($body.find(selector).length > 0) {
+                    cy.get(selector).then($events => {
+                        var randomEvent = $events.get(getRandomInt(0, $events.length));
+                        cy.wrap(randomEvent).clear();
+                        cy.wrap(randomEvent).type('test');
+                        cy.wait(500);
+                    });
+                }
+            });
+        }
+    });
+}
